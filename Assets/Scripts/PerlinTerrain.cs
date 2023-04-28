f88f356e9e63485569ca31d60101bbf98fd075ee
JoniHelen <djohe015@edu.xamk.fi>
2023-04-29 01:04:47 +0300

    amplitude and other things

--- Assets/Scripts/PerlinTerrain.cs
+++ Assets/Scripts/PerlinTerrain.cs
@@ -22,35 +22,48 @@ public class PerlinTerrain : MonoBehaviour
     struct ParallelVertexJob : IJobParallelFor
     {
         [ReadOnly]
-        public NativeArray<int> SubdivisionCount;
+        public NativeArray<float> ObjParams;
 
         public NativeArray<Vertex> result;
 
         public void Execute(int i)
         {
-            int vertexStride = (int)(Mathf.Pow(2, SubdivisionCount[0]) + 1);
+            int vertexStride = (int)(Mathf.Pow(2, ObjParams[0]) + 1);
             float y = i / vertexStride;
             float x = i % vertexStride;
 
-            y = Remap(y, 0, vertexStride, -0.5f, 0.5f);
-            x = Remap(x, 0, vertexStride, -0.5f, 0.5f);
+            y = Remap(y, 0, vertexStride, 0, 1);
+            x = Remap(x, 0, vertexStride, 0, 1);
+
+            Vector3 pos = new Vector3(x - 0.5f, Mathf.PerlinNoise(x * 10, y * 10) * ObjParams[1], y - 0.5f);
+            Vector3 normal = NormalFromHeight(new Vector2(x * 10, y * 10), ObjParams[1]);
 
             result[i] = new Vertex()
             {
-                position = new Vector3(x, 0, y),
-                normal = Vector3.up,
+                position = pos,
+                normal = normal,
                 tangent = new Vector4(1, 0, 0, 1),
-                texCoord0 = new Vector2(Remap(x, -0.5f, 0.5f, 0, 1), Remap(y, -0.5f, 0.5f, 0, 1))
+                texCoord0 = new Vector2(x, y)
             };
         }
     }
 
     NativeArray<uint> IndexResult;
 
+    private static Vector3 NormalFromHeight(Vector2 id, float amplitude)
+    {
+        float L = Mathf.PerlinNoise(id.x - 0.0001f, id.y) * amplitude;
+        float R = Mathf.PerlinNoise(id.x + 0.0001f, id.y) * amplitude;
+        float T = Mathf.PerlinNoise(id.x, id.y + 0.0001f) * amplitude;
+        float B = Mathf.PerlinNoise(id.x, id.y - 0.0001f) * amplitude;
+
+        return -new Vector3(2 * (R - L), -4, 2 * (B - T)).normalized;
+    }
+
     struct ParallelIndexJob : IJobParallelForBatch
     {
         [ReadOnly]
-        public NativeArray<int> SubdivisionCount;
+        public NativeArray<float> ObjParams;
 
         public NativeArray<uint> result;
 
@@ -59,7 +72,7 @@ public class PerlinTerrain : MonoBehaviour
             // count is always 6 for processing quads
             int i = startIndex / count;
 
-            int quadStride = (int)Mathf.Pow(2, SubdivisionCount[0]);
+            int quadStride = (int)Mathf.Pow(2, ObjParams[0]);
             int y = i / quadStride;
 
             int vertTopLeft = i + y;
@@ -80,9 +93,17 @@ public class PerlinTerrain : MonoBehaviour
     [SerializeField, Range(0, 10)]
     private int SubdivisionCount = 0;
 
+    [SerializeField, Range(0.1f, 100f)]
+    private float Amplitude = 1;
+
     [SerializeField]
     private MeshFilter meshFilter;
 
+
+    private void OnValidate()
+    {
+        GenerateMesh();
+    }
 
     private static float Remap(float value, float low1, float high1, float low2, float high2)
         => low2 + (value - low1) * (high2 - low2) / (high1 - low1);
@@ -103,20 +124,21 @@ public class PerlinTerrain : MonoBehaviour
         float tc = Mathf.Pow(4, SubdivisionCount);
         int triangleIndexCount = (int)(6 * tc);
 
-        NativeArray<int> subCount = new NativeArray<int>(1, Allocator.TempJob);
-        subCount[0] = SubdivisionCount;
+        NativeArray<float> variables = new NativeArray<float>(2, Allocator.TempJob);
+        variables[0] = SubdivisionCount;
+        variables[1] = Amplitude;
         VertexResult = new NativeArray<Vertex>(vertexCount, Allocator.TempJob);
         IndexResult = new NativeArray<uint>(triangleIndexCount, Allocator.TempJob);
 
         ParallelVertexJob vertJobData = new ParallelVertexJob();
-        vertJobData.SubdivisionCount = subCount;
+        vertJobData.ObjParams = variables;
         vertJobData.result = VertexResult;
 
         ParallelIndexJob indexJobData = new ParallelIndexJob();
-        indexJobData.SubdivisionCount = subCount;
+        indexJobData.ObjParams = variables;
         indexJobData.result = IndexResult;
 
-        JobHandle vertHandle = vertJobData.Schedule(vertexCount, 1);
+        JobHandle vertHandle = vertJobData.Schedule(vertexCount, 16);
         JobHandle indexHandle = indexJobData.ScheduleBatch(triangleIndexCount, 6);
 
         Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(1);
@@ -166,7 +188,9 @@ public class PerlinTerrain : MonoBehaviour
         Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, meshFilter.sharedMesh);
 
         meshFilter.sharedMesh.RecalculateBounds();
+        meshFilter.sharedMesh.RecalculateNormals();
+        meshFilter.sharedMesh.RecalculateTangents();
 
-        subCount.Dispose();
+        variables.Dispose();
     }
 }
